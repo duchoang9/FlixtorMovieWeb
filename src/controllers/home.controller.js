68494058277d51cc9a0d6cef1d0dd8b9c2d9106a
thanghh151 <21020130@vnu.edu.vn>
2023-04-25 17:57:01 +0700

    home 25/04

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -1,40 +1,43 @@
 const { movie_list, movie, watch_list } = require('../models');
 const { Op } = require('sequelize');
+
 // GET /
 module.exports.home = async (req, res) => {
-    try {
-      const hotMovies = await movie.findAll({
-        order: [['rating', 'DESC']],
-        limit: 8
-      });
-  
-      const latestMovies = await movie.findAll({
-        order: [['release', 'DESC']],
-        limit: 8
-      });
-  
-      let watch_list = null;
-      if (req.user) {
-        watch_list = await watch_list.findOne({
-          where: { user_id: req.user.id },
-          include: [{
-            model: movie_list,
-            include: [{ model: movie }]
-          }]
-        });
-      }
-  
-      res.render('home', {
-        title: 'Home',
-        hotMovies,
-        latestMovies,
-        watch_list
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).send('Server Error');
-    }
-  };
+  try {
+    const hotMovies = await movie.findAll({
+      order: [['rating', 'DESC']],
+      limit: 8
+    });
+
+    const latestMovies = await movie.findAll({
+      order: [['create_at', 'DESC']],
+      limit: 8
+    });
+
+    const movieInWatchList = await movie.findAll({ 
+      include: [
+          {
+              model: watch_list,
+              where: {user_id: req.userId}
+          }
+      ],
+      raw: true
+    });
+
+    res.render('home', {
+      title: 'Home',
+      hotMovies,
+      latestMovies,
+      movieInWatchList
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+};
+
+
 // GET /search
 module.exports.search = async (req, res) => {
   const { keyword } = req.query;
--- src/routes/home.route.js
+++ src/routes/home.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('../controllers/home.controller');
+const { verifyToken, isUser } = require('../middlewares/authJwt');
 
-router.get('/', controller.home);
+router.get('/', [verifyToken, isUser], controller.home);
 router.get('/search', controller.search);
 
 module.exports = router;
